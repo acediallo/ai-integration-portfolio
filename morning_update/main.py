b9ac087862a723d9a042fec473c90295494a3bbd
acediallo <diallo.amadoucolly@gmail.com>
2025-08-30 12:35:02 +0000

    adding news api client

--- morning_update/main.py
+++ morning_update/main.py
@@ -1,6 +1,15 @@
-from openai_client import generate_text
+#from openai_client import generate_text
+from news_client import get_news_headlines
 
 
 if __name__ == "__main__":
-    update = generate_text("Give me a short morning update about global stock markets.")
-    print(update) 
+    
+    #testing open ai api call to generate a text from a prompt
+    #update = generate_text("Give me a short morning update about global stock markets.")
+    #print(update) 
+
+    #testing news api 
+    headlines_list = get_news_headlines()[:5]  #get only first 5 headlines
+    print(headlines_list)
+
+
--- morning_update/news_client.py
+++ morning_update/news_client.py
@@ -10,37 +10,53 @@ NEWSAPI_TOP_HEADLINES_URL = "https://newsapi.org/v2/top-headlines"
 
 def get_news_headlines () :
     headline_articles = []
-    newsapi_url_params = {"country" : "sa",
-                        "pageSize" : 5,
+    newsapi_url_params = {"country" : "us",
+                        #"pageSize" : 5,
                         }
     newsapi_headers = {
-        "Authorization": f" bearer {API_KEY_NEWSAPI}",
-        "Content-Type": "application/json"
+        #"Authorization": f"Bearer {API_KEY_NEWSAPI}",
+        "X-Api-Key": API_KEY_NEWSAPI        
     }
     
     try: 
-        response = requests.get(NEWSAPI_TOP_HEADLINES_URL, newsapi_url_params, newsapi_headers, timeout=30)
+        response = requests.get(NEWSAPI_TOP_HEADLINES_URL, params=newsapi_url_params, headers=newsapi_headers, timeout=30)
         response.raise_for_status() #raises HTTPError for bad status codes
+        data = response.json()
         #return response
+        
+        #print(response.status_code) #debugging line to see the status code
+        #print(response.json())  #debugging line to see the full response
+        print("*" * 20)
+        print(data.keys())  #debugging line to see the keys in the response
+        print("*" * 20)
+        print(data["articles"][0])  #debugging line to see the first article in the response
+        print("*" * 20)
+        print(len(data["articles"]))  #debugging line to see how many articles were returned
+        
+        #now let's retrieve list of articles with only title and description
+        for article in data["articles"]: 
+            headline_articles.append({
+                "title": article.get("title","No Title"), #use get method to avoid KeyError if key is missing
+                "description":article.get("description","No Description")
+                })
+
+        return headline_articles 
+
+
     except requests.exceptions.ConnectionError:
         print("Network connection failed")
-        return None
     except requests.exceptions.Timeout:
         print("Request timed out")
-        return None
     except requests.exceptions.HTTPError as e:
         print(f"HTTP error {e.response.status_code}: {e}")
-        return None
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-        return None
+    return []
 
     #another way to implement bad status code 
     #    if response.status_code != 200:
     #       sys.exit(f"Error: Unexpected status code {response.status_code}")
     
-    #now let's retrieve list of articles with only title and description
-    for article in response.articles :
-        headline_articles.append({"title":article.tilte, "description":article.description})
 
-    return headline_articles
+
+    
